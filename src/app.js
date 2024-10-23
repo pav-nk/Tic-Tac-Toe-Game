d83691d19294039aec8fbe52eca312fcc00bbfd4
pav_nk <pav_nk@vk.com>
2024-10-23 17:29:22 +0300

    feat: the functionality of checking the victory condition has been added

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ function Gameboard() {
     const rows = 3;
     const columns = 3;
     const board = [];
+    const hashIdBoard = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+    ];
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push(Cell(i, j));
+            board[i].push(Cell(i, j, hashIdBoard[i][j]));
         }
     }
 
@@ -30,7 +35,7 @@ function Gameboard() {
     return { getBoard, setToken, printBoard };
 }
 
-function Cell(x, y) {
+function Cell(x, y, hashId) {
     let value = "-";
 
     const addToken = (token) => {
@@ -43,10 +48,15 @@ function Cell(x, y) {
         return { x, y };
     };
 
+    const getHashId = () => {
+        return hashId;
+    };
+
     return {
         addToken,
         getValue,
         getCoordinates,
+        getHashId,
     };
 }
 
@@ -79,6 +89,35 @@ function GameController(
         console.log(`${getActivePlayer().name}'s turn.`);
     };
 
+    const isVictory = () => {
+        const combs = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let comb of combs) {
+            const currentBoardLine = comb.map((item) => {
+                const [currentCell] = board
+                    .getBoard()
+                    .flat()
+                    .filter((cell) => item === cell.getHashId());
+                return currentCell;
+            });
+            const currentBoardLineTokens = currentBoardLine
+                .map((cell) => cell.getValue())
+                .join("");
+            if (currentBoardLineTokens === ("XXX" || "000")) return true;
+        }
+
+        return false;
+    };
+
     const playRound = (coordinates) => {
         console.log(
             `${getActivePlayer().name} set the token by coordinates ${
@@ -86,16 +125,21 @@ function GameController(
             } and ${coordinates.y}`
         );
         board.setToken(coordinates, getActivePlayer().token);
-
-        switchPlayerTurn();
-        printNewRound();
+        if (!isVictory()) {
+            switchPlayerTurn();
+            printNewRound();
+        } else {
+            console.log(`${getActivePlayer().name} is the winner!`);
+            board.printBoard();
+            return true;
+        }
+        return false;
     };
 
     printNewRound();
 
     return {
         playRound,
-        getActivePlayer,
     };
 }
 
@@ -104,12 +148,15 @@ function ScreenController() {}
 function clickHandlerBoard(evt) {}
 
 function runGame() {
-    const board = Gameboard();
     const { playRound } = GameController();
     while (true) {
         const indexX = readlineSync.question("Choose the X coordinate!");
         const indexY = readlineSync.question("Choose the Y coordinate!");
-        playRound({ x: Number(indexX) - 1, y: Number(indexY) - 1 });
+        const isGameOver = playRound({ x: Number(indexX), y: Number(indexY) });
+        if (isGameOver) {
+            console.log("Game over!");
+            return;
+        }
     }
 }
 
